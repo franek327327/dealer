2aa02c6f78d8d4bcbbe20ae1218d1809eaf1fb15
franek327327 <franek327@wp.pl>
2021-11-17 12:52:29 +0100

    ok

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>JavaScript</title>
+        <title>Dealer samochodowy</title>
         <script src="app.js"></script>
         <link rel="stylesheet" href="style.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
