edbf80633217abee49ee3442363532cad875aa0f
filipek <filip.piszczan@gmail.com>
2021-11-17 12:45:24 +0100

    dsa

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
                 <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
                 <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
               </ul>
-            </div><h2></h2>
+            </div>
           </nav>
 		  
 		  
